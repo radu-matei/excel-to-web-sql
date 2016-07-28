954082811058213fe799c10043a9b78390b00672
Radu Matei <matei.radu94@gmail.com>
2016-07-28 19:57:33 +0300

    Modified function to accept and use the statement from a text file

--- src/PowerShell Scripts/Execute-SqlStatement.ps1
+++ src/PowerShell Scripts/Execute-SqlStatement.ps1
@@ -5,6 +5,7 @@
 #
 #.DESCRIPTION
 # This function uses the SqlClient to execute statements against a database.
+# The statement can come as a string or as a text file on the disk
 # 
 # If $StatementType is "Query", it will use the SqlDataAdapter to get the data
 # and display all returned tables.
@@ -19,14 +20,22 @@
 # and represents the type of the statement to be executed
 #
 #.PARAMETER $SqlStatement
-# This parameter contains the actual SQL statement to be executed 
+# This parameter contains the actual SQL statement to be executed. If this parameter is used,
+# then $UseScriptFile and $ScriptFilePath are no longer required
 #
+#.PARAMETER $UseScriptFile
+# Switch parameter to decide the source of the SQL statement. If present, this switch will require the 
+# $ScriptFilePath parameter and will make $SqlStatement not needed.   
 #
 #.EXAMPLE
 # Execute-SqlStatement -ConnectionString $databaseConnectionString -StatementType Query -SqlStatement "SELECT * FROM TableName"
 #
 #.EXAMPLE
 # Execute-SqlStatement -ConnectionString $databaseConnectionString -StatementType Command -SqlStatement "DROP TABLE TableName"
+#
+#.EXAMPLE
+# Execute-SqlStatement -ConnectionString $databaseConnectionString -StatementType Command -UseScriptFile -ScriptFilePath $commandScriptPath
+#
 ##############################################################################
 
 function Execute-SqlStatement
@@ -37,11 +46,20 @@ function Execute-SqlStatement
         [ValidateSet("Query", "Command")]
         [string]$StatementType,
         
-        [string]$SqlStatement
+        [Parameter(ParameterSetName="NoFileRequired")]
+        [string]$SqlStatement,
+
+        [Parameter(ParameterSetName="FileRequired")]
+        [switch]$UseScriptFile,
+
+        [Parameter(ParameterSetName="FileRequired")]
+        [string]$ScriptFilePath
         )
 
+    $statementText = if($UseScriptFile) { (Get-Content -Path $createScript) | Out-String } else { $SqlStatement }
+
     $connection = New-Object System.Data.SqlClient.SqlConnection($ConnectionString)
-    $command = New-Object System.Data.SqlClient.SqlCommand($SqlStatement, $connection)
+    $command = New-Object System.Data.SqlClient.SqlCommand($statementText, $connection)
     $connection.Open()
 
     if($StatementType -eq "Query")
