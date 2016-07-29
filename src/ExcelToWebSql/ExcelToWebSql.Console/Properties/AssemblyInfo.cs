445f8d66811b020705847130a7a2166015956b40
Radu Matei <matei.radu94@gmail.com>
2016-07-29 18:30:53 +0300

    Renamed project

--- src/ExcelToWebSql/ExcelToWebSql.Console/Properties/AssemblyInfo.cs
+++ src/ExcelToWebSql/ExcelToWebSql.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelToWebSql.Console")]
+[assembly: AssemblyTitle("ExcelToWebSql.Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelToWebSql.Console")]
+[assembly: AssemblyProduct("ExcelToWebSql.Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
