1be69d2aac0b50205ce656dd5d36c48a528a7812
Radu Matei <matei.radu94@gmail.com>
2016-07-29 18:31:35 +0300

    Added small example for using the script generator

--- src/ExcelToWebSql/ExcelToWebSql.Console/Program.cs
+++ src/ExcelToWebSql/ExcelToWebSql.Console/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ExcelToWebSql.Console
+﻿namespace ExcelToWebSql.Generator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var path = @"C:\Users\t-ramate\Documents\Visual Studio 2015\Projects\ConsoleExcelDataReader\ConsoleExcelDataReader\worksheet.xlsx";
+            IExcelSqlScriptGenerator scriptGenerator = new ExcelSqlScriptGenerator(path);
+
+            scriptGenerator.GenerateSqlTableScipts();
+            scriptGenerator.GenerateSqlInsertScripts();
+
+            scriptGenerator.Dispose();
         }
     }
 }
