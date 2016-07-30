f2ddd9062c14d36f27c8f5b81159095a560fd5b3
Radu Matei <matei.radu94@gmail.com>
2016-07-30 16:29:26 +0300

    Added usage example for the converter

--- src/ExcelToWebSql/ExcelToWebSql.Console/Program.cs
+++ src/ExcelToWebSql/ExcelToWebSql.Console/Program.cs
@@ -5,12 +5,17 @@
         static void Main(string[] args)
         {
             var path = @"C:\Users\t-ramate\Documents\Visual Studio 2015\Projects\ConsoleExcelDataReader\ConsoleExcelDataReader\worksheet.xlsx";
-            IExcelSqlScriptGenerator scriptGenerator = new ExcelSqlScriptGenerator(path);
+            //IExcelSqlScriptGenerator scriptGenerator = new ExcelSqlScriptGenerator(path);
 
-            scriptGenerator.GenerateSqlTableScipts();
-            scriptGenerator.GenerateSqlInsertScripts();
+            //scriptGenerator.GenerateSqlTableScipts();
+            //scriptGenerator.GenerateSqlInsertScripts();
 
-            scriptGenerator.Dispose();
+            //scriptGenerator.Dispose();
+
+            IExcelConverter excelConverter = new ExcelConverter(path);
+            excelConverter.ConvertToJson(path);
+
+            excelConverter.Dispose();
         }
     }
 }
