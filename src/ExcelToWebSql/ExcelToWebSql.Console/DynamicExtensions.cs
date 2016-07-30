bd1de69d924db322dad6c5b634328c0c56be5935
Radu Matei <matei.radu94@gmail.com>
2016-07-30 16:27:46 +0300

    Added static class for extension methods for dynamic objects

---
+++ src/ExcelToWebSql/ExcelToWebSql.Console/DynamicExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ExcelToWebSql.Generator
+{
+    public static class DynamicExtensions
+    {
+        public static void AddProperty(this ExpandoObject dynamicObject, string propertyName, object propertyValue)
+        {
+            var dynamicDictionary = dynamicObject as IDictionary<string, object>;
+
+            if (dynamicDictionary.ContainsKey(propertyName))
+                dynamicDictionary[propertyName] = propertyValue;
+
+            else
+                dynamicDictionary.Add(propertyName, propertyValue);
+        }
+
+        public static void Print(this ExpandoObject dynamicObject)
+        {
+            var dynamicDictionary = dynamicObject as IDictionary<string, object>;
+
+            foreach(KeyValuePair<string, object> property in dynamicDictionary)
+            {
+                Console.WriteLine("{0}: {1}", property.Key, property.Value.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
