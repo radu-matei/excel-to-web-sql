7af8f2f3037a4f4978e2dc7f5863ce020bc08aae
Radu Matei <matei.radu94@gmail.com>
2016-08-01 09:56:14 +0300

    Renamed project folder to match project name

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExcelToWebSql.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFilePath = @"C:\Users\t-ramate\Documents\Visual Studio 2015\Projects\ConsoleExcelDataReader\ConsoleExcelDataReader\worksheet.xlsx";
+            //IExcelSqlScriptGenerator scriptGenerator = new ExcelSqlScriptGenerator(sourceFilePath);
+
+            //scriptGenerator.GenerateSqlTableScipts();
+            //scriptGenerator.GenerateSqlInsertScripts();
+
+            //scriptGenerator.Dispose();
+
+            IExcelConverter excelConverter = new ExcelConverter(sourceFilePath);
+            excelConverter.ConvertToJson(sourceFilePath);
+
+            excelConverter.Dispose();
+        }
+    }
+}
