e9952eb781ad07f67a605db88b68a2f843704e9c
Radu Matei <matei.radu94@gmail.com>
2016-08-01 12:43:33 +0300

    Refactored IExcelConverter interface

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Converter/IExcelConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExcelToWebSql.Generator
+{
+    public interface IExcelConverter : IDisposable
+    {
+        void ConvertToJson(string sourceFilePath, string outputFilePath);
+        void ConvertToXml(string sourceFilePath, string outputFilePath);
+    }
+}
+
