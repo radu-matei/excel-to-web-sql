98631da70c997025a0e76e43957601634196f99a
Radu Matei <matei.radu94@gmail.com>
2016-08-01 12:45:47 +0300

    Added simple storage implementation

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Storage/FileStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelToWebSql.Generator.Storage
+{
+    public class FileStorage : IStorage
+    {
+        public void SaveDocument(string content, string outputFilePath)
+        {
+            try
+            {
+                using (var stream = File.Open(outputFilePath, FileMode.Create, FileAccess.Write))
+                using (var sw = new StreamWriter(stream))
+                {
+                    sw.Write(content);
+                    sw.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw new AccessViolationException();
+            }
+        }
+    }
+}
