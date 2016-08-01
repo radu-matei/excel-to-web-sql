91308a2f6ea34ee11000028da41a76f8dd1560cc
Radu Matei <matei.radu94@gmail.com>
2016-08-01 12:41:40 +0300

    Added xml serialization implementation

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Serializer/XmlSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ExcelToWebSql.Generator
+{
+    public class XmlSerializer : ISerializer
+    {
+        public string Serialize(object sourceObject)
+        {
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(sourceObject.GetType());
+            using (StringWriter writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, sourceObject);
+                return writer.ToString();
+            }
+        }
+    }
+}
