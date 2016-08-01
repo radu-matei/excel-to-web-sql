5b68eda7701e43b8361967a6fa174486df9376fb
Radu Matei <matei.radu94@gmail.com>
2016-08-01 12:46:12 +0300

    Refactored IExcelSqlScriptGenerator

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Generator/IExcelSqlScriptGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExcelToWebSql.Generator
+{
+    public interface IExcelSqlScriptGenerator : IDisposable
+    {
+        void GenerateSqlTableScipts(string sourceFilePath, string outputFilePath);
+        void GenerateSqlInsertScripts(string sourceFilePath, string outputFilePath);
+    }
+}
