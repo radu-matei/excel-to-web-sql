a3d1d329827e714832880b27a33dc315530296fe
Radu Matei <matei.radu94@gmail.com>
2016-08-01 12:41:55 +0300

    Added json serialization implementation

---
+++ src/ExcelToWebSql/ExcelToWebSql.Generator/Serializer/JsonSerializer.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ExcelToWebSql.Generator
+{
+    public class JsonSerializer : ISerializer
+    {
+        public string Serialize(object sourceObject)
+        {
+            return JsonConvert.SerializeObject(sourceObject);
+        }
+    }
+}
